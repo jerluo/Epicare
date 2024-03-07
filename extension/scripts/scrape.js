17e7792c2920d9d0dfdef6da665286fb5c0d6f82
brocknaidl <bnaidl@wisc.edu>
2024-03-07 12:38:53 -0600

    Added starter code for google gemini integration

--- extension/scripts/scrape.js
+++ extension/scripts/scrape.js
@@ -1,5 +1,16 @@
 console.log("scrape script loaded");
 
+// Creating gemini process and its starter code
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const dotenv = require('dotenv');
+dotenv.config();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI);
+
+
 // checking for the element to see if it exists so it doesnt stop functioning.
 const fmtConvElement = document.querySelector(".fmtConv");
 if (fmtConvElement) {
